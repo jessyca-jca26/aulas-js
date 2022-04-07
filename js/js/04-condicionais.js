6e56524fb7516f6f4502ecbf33607c3f256d2fcf
jessyca-jca26 <jessyca.jca26@gmail.com>
2022-04-07 17:02:52 -0300

    condicional

---
+++ js/js/04-condicionais.js
@@ -0,0 +1,65 @@
+/* Estruturas Condional
+if (se), else (senão) else if (senão se) 
+
+Operadores relacionais (usados com condicionais)
+> maior que 
+< menor que 
+>= maior ou igual
+<= menor ou igual
+== igualdade 
+!= diferente*/
+
+
+// Exemplo 1 : condicional SIMPLES 
+let numero = 10;
+
+if (numero > 0) {
+    console.log(`${numero} é maior que zero`);
+
+}
+
+// Exemplo 2: condicional COMPOSTA (IF/ELSE)
+
+let aluno= "João";
+let nota1= 8;
+let nota2= 7;
+let media=(nota1 + nota2 ) / 2;
+
+console.log(`Aluno: ${aluno}`);
+console.log(`Notas: ${nota1}e ${nota2}`);
+console.log(`Média ${media.toFixed(2)}`);
+
+if(media >= 7){
+    console.log("aprovado");
+
+} else{
+    console.log("reprovado");
+
+}
+
+console.log(situacao) // undefined ;
+
+// Exemplo 3: condicional ENCADEADA (IF, ELSE IF,ELSE)
+let desempenho;
+if(media < 3){
+    desempenho= "péssimo";
+
+} else if(media < 5){
+    desempenho="ruim";
+
+} else if(media<7){
+    desempenho= "regular";
+
+}else if(media <9){
+    desempenho="bom";
+
+} else{
+    desempenho="otimo"
+
+}
+
+console.log(desempenho);
+
+
+
+
